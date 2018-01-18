9c4952f4c44aeeae3e6f719b2aaecc748689ca56
Cosmaty1991 <cosmaty1991@gmail.com>
2018-01-18 15:55:59 +0200

    Initial commit

--- js/dest/script.js
+++ js/dest/script.js
@@ -330,6 +330,17 @@ $(function() {
   });
 });
 
+// internet explorer hover fix
+
+for (var i = 0; i < document.querySelectorAll('.header-menu__item').length; i++) {
+  document.querySelectorAll('.header-menu__item')[1].addEventListener('mouseover', function(){
+    document.querySelector('.rooms__list').style.display = 'block';
+  });
+  
+  document.querySelector('.rooms__list').addEventListener('mouseout', function(){
+    this.style.display = 'none';
+  });
+}
 // booking luxe rooms
 
 $(function() {
