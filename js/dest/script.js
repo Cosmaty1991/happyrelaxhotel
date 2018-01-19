a327782ba8a2544ed99421c89d10a024e711fa93
Cosmaty1991 <cosmaty1991@gmail.com>
2018-01-19 15:48:23 +0200

    Initial commit

--- js/dest/script.js
+++ js/dest/script.js
@@ -769,7 +769,7 @@ function Tabs() {
   var clear = function() {
     var menuElements = document.querySelectorAll('[data-tab]');
 
-    for (var i = 0; i < menuElements.length ; i++) {
+    for (var i = 0; i < menuElements.length; i++) {
       menuElements[i].classList.remove('active');
 
       var id = menuElements[i].getAttribute('data-tab');
